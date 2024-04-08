f7591665af4414e39c6f4f952bad339556891903
Auguspo <augustpolet@gmail.com>
2024-04-08 17:24:41 -0300

    revalidation added

---
+++ src/pages/api/revalidate.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  await res.revalidate("/");
+  return res.json({ revalidated: true });
+}
